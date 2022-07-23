45f56afb6824c3ca57fb2a06c2565d2eb02ca77b
Gabriel-P22 <paschoalfilho0303@gmail.com>
2022-07-23 14:14:41 -0300

    Implement fake persistence and created tests

---
+++ src/persistence/TaskPersistence/TaskPersistence.js
@@ -0,0 +1,21 @@
+const TaskList = require("../../model/taskList/TaskList")
+
+class TaskPersistence {
+    constructor() {
+        this._taskList = new TaskList()
+    }
+
+    save(task) {
+        this._taskList.add(task)
+    }
+
+    find(description) {
+        return this._taskList.search(description)
+    }
+
+    listAll() {
+        return this._taskList.listAll()
+    }
+}
+
+module.exports = TaskPersistence 
---
+++ src/persistence/TaskPersistence/TaskPersistence.spec.js
@@ -0,0 +1,28 @@
+const Task = require("../../model/task/Task.js")
+
+const TaskPersistence = require("./TaskPersistence.js")
+
+describe("TaskPersistence", () => {
+    let taskPersistence
+
+    beforeEach(() => {
+        taskPersistence = new TaskPersistence()
+    })
+
+
+    test("When a task is recovered from the database, its data must be the same as when it was saved.", () => {
+        const task = new Task("I'm studying")
+        taskPersistence.save(task)
+        let taskResponse = taskPersistence.find("I'm studying")
+        expect(task).toBe(taskResponse[0])
+    })
+    
+    test("Can list all tasks in the database.", () => {
+        const firstTask = new Task("Take the dog to the vet")
+        const secondTask = new Task("Read one chapter of the clean architecture book")
+        taskPersistence.save(firstTask)
+        taskPersistence.save(secondTask)
+        let list = taskPersistence.listAll()
+        expect([firstTask, secondTask]).toStrictEqual(list)
+    })
+});
